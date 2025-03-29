873554b513b7e144992888471942a5145cfe9a34
Kailie Le <davn0376@gmail.com>
2025-03-29 17:04:13 -0400

    Update iris_classification.py
    Signed-off-by: Kailie Le <davn0376@gmail.com>

--- iris_classification.py
+++ iris_classification.py
@@ -10,7 +10,7 @@ iris = load_iris()
 # Features (X) and target (y)
 X = iris.data
 y = iris.target
-
+ 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
